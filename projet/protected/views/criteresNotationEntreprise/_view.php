0a23313bd0dce2966126d18a77c4da755681e99e
vanhovea <vanhove.alexiss@gmail.com>
2017-03-07 10:53:00 +0100

    Ajout de projet

---
+++ projet/protected/views/criteresNotationEntreprise/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this CriteresNotationEntrepriseController */
+/* @var $data CriteresNotationEntreprise */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id_critere_entreprise')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id_critere_entreprise), array('view', 'id'=>$data->id_critere_entreprise)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nom_critere_entreprise')); ?>:</b>
+	<?php echo CHtml::encode($data->nom_critere_entreprise); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('critere_note_entreprise')); ?>:</b>
+	<?php echo CHtml::encode($data->critere_note_entreprise); ?>
+	<br />
+
+
+</div>
---
+++ projet/protected/views/layouts/main.php
@@ -0,0 +1,100 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="en">
+
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container" id="page">
+
+	<div id="header">
+		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+	</div><!-- header -->
+
+	<div id="mainmenu">
+
+		<?php 
+
+		//On récupère les données de l'utilisateur qui est sur le site
+		$user = Yii::app()->user;
+		$utilisateur = Utilisateur::model()->FindByAttributes(array('login'=>$user->id));
+		
+		//Si c'est un employé, on lui affiche certains onglets
+		if($user->getState('type') == "employe")
+		{
+
+			$this->widget('zii.widgets.CMenu',array(
+			'items'=>array(
+				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Employe', 'url'=>array('/employe/view', 'id'=>$utilisateur->id_employe)),
+				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				),
+			));
+		}
+		//Si c'est une entreprise, on lui affiche certains autres onglets
+		else if($user->getState('type') == "entreprise")
+		{
+			$this->widget('zii.widgets.CMenu',array(
+			'items'=>array(
+				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Entreprise', 'url'=>array('/entreprise/view', 'id'=>$utilisateur->id_entreprise)),
+				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				),
+			));
+		}
+		//Si c'est un invité, il n'a que la connexion
+		else
+		{
+			$this->widget('zii.widgets.CMenu',array(
+			'items'=>array(
+				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				),
+			));
+		}
+
+		 
+		
+		?>
+
+
+	</div><!-- mainmenu -->
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+			'links'=>$this->breadcrumbs,
+		)); ?><!-- breadcrumbs -->
+	<?php endif?>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer">
+		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		All Rights Reserved.<br/>
+		<?php echo Yii::powered(); ?>
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
