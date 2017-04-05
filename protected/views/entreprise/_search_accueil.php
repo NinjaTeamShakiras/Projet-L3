26f7311240f31f5b3a53a61ff51b9bf91299dbe6
Rouxfoul <thomas.georget.reglin@gmail.com>
2017-04-05 13:17:32 +0200

    YOLO

--- protected/views/entreprise/_search_accueil.php
+++ protected/views/entreprise/_search_accueil.php
@@ -10,12 +10,15 @@
 
 $adresse = Adresse::model()->findAll();
 
+var_dump(Yii::app()->user->getState('type'));
+
 $form=$this->beginWidget('CActiveForm', array(
-	'action'=>Yii::app()->createUrl('Entreprise/search'),
+	'action'=>Yii::app()->createUrl('Entreprise/Search'),
 	'method'=>'get',
 )); ?>
 
 	<div class="row" align='center'>
+		<p>Ne fonctionne pas, à voir ! </p>
 		<?php 
 		echo $form->textField($model,'nom_entreprise',array('size'=>45,'maxlength'=>45, 'value'=>'Rechercher une entreprise')); ?>
 		<?php echo CHtml::submitButton('Rechercher'); ?>
