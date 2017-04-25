ca58305e05080e066ac8803f30707bb22f7cb611
culeren <kcnarfy-m.73@hotmail.fr>
2017-04-25 13:26:28 +0200

    %1

--- protected/views/offreEmploi/index.php
+++ protected/views/offreEmploi/index.php
@@ -57,8 +57,8 @@
 
 			if($value->id_entreprise == $utilisateur->id_entreprise) // Si l'id de l'entreprise de l'offre correspond à l'idée de l'entreprise logée
 			{
-				print("<p> ID entreprise : ".$value->id_entreprise."</p>");
-				print("<p> ID offre : ".$value->id_offre_emploi."</p>");
+				//print("<p> ID entreprise : ".$value->id_entreprise."</p>");
+				//print("<p> ID offre : ".$value->id_offre_emploi."</p>");
 				print("<p> Date de mise en ligne : ".$this->changeDateNaissance($value->date_creation_offre_emploi)."</p>");
 				print("<p> Type de l'offre : ".$value->type_offre_emploi."</p>");
 				print("<p> Sallaire proposé : ".$value->salaire_offre_emploi." €</p>");
@@ -74,17 +74,29 @@
 	}
 	else if( Utilisateur::est_employe(Yii::app()->user->role))
 	{  // Si employé on affiche toutes les offres d'emploi
+		$tablePostuler = Postuler::model()->FindAll();
+		
 
-		foreach ($model as $key => $value ) //  Pour chaque offre on affiche :
+		foreach ($model as $key => $offre ) //  Pour chaque offre on affiche :
 		{
-			print("<p> ID entreprise : ".$value->id_entreprise."</p>");
-			print("<p> ID offre : ".$value->id_offre_emploi."</p>");
-			print("<p> Date de mise en ligne : ".$this->changeDateNaissance($value->date_creation_offre_emploi)."</p>");
-			print("<p> Type de l'offre : ".$value->type_offre_emploi."</p>");
-			print("<p> Sallaire proposé : ".$value->salaire_offre_emploi." €</p>");
-			print("<p> Expérience nécéssaire : ".$value->experience_offre_emploi."</p>");
-			print("<p> Description de l'offre : ".$value->description_offre_emploi."</p>");
-			echo CHtml::link('Voir cette offre' ,array('offreEmploi/view', 'id'=>$value->id_offre_emploi));
+			$entreprise = entreprise::model()->FindByAttributes(array("id_entreprise"=>$offre->id_entreprise)); // On récupère l'entreprise qui propose l'offre
+
+			//print("<p> ID entreprise : ".$offre->id_entreprise."</p>");
+			//print("<p> ID offre : ".$offre->id_offre_emploi."</p>");
+			print("<p> Proposé par : ".$entreprise->nom_entreprise."</p>");
+			print("<p> Date de mise en ligne : ".$this->changeDateNaissance($offre->date_creation_offre_emploi)."</p>");
+			print("<p> Type de l'offre : ".$offre->type_offre_emploi."</p>");
+			print("<p> Sallaire proposé : ".$offre->salaire_offre_emploi." €</p>");
+			print("<p> Expérience nécéssaire : ".$offre->experience_offre_emploi."</p>");
+			print("<p> Description de l'offre : ".$offre->description_offre_emploi."</p>");
+			foreach($tablePostuler as $postuler)
+			{
+				if($postuler->id_employe == $utilisateur->id_employe && $postuler->id_offre_emploi == $offre->id_offre_emploi )
+				{
+					print("<p> Vous avez postuler à cette offre le : ".$this->changeDateNaissance($postuler->date_postule)."</p>");
+				}
+			}
+			echo CHtml::link('Voir cette offre' ,array('offreEmploi/view', 'id'=>$offre->id_offre_emploi));
 			echo "<hr/>";
 		}
 
--- protected/views/offreEmploi/mesOffres.php
+++ protected/views/offreEmploi/mesOffres.php
@@ -33,14 +33,17 @@
 		if($postuler->id_employe == $utilisateur->id_employe)
 		{
 			$offre = OffreEmploi::model()->FindByAttributes(array("id_offre_emploi"=>$postuler->id_offre_emploi)); // On récupère l'offre concernée
+			$entreprise = entreprise::model()->FindByAttributes(array("id_entreprise"=>$offre->id_entreprise)); // On récupère l'entreprise qui propose l'offre
 
-			print("<p> ID entreprise : ".$offre->id_entreprise."</p>");
-			print("<p> ID offre : ".$offre->id_offre_emploi."</p>");
+			//print("<p> ID entreprise : ".$offre->id_entreprise."</p>");
+			//print("<p> ID offre : ".$offre->id_offre_emploi."</p>");
+			print("<p> Proposé par : ".$entreprise->nom_entreprise."</p>");
 			print("<p> Date de mise en ligne : ".$this->changeDateNaissance($offre->date_creation_offre_emploi)."</p>");
 			print("<p> Type de l'offre : ".$offre->type_offre_emploi."</p>");
 			print("<p> Sallaire proposé : ".$offre->salaire_offre_emploi." €</p>");
 			print("<p> Expérience nécéssaire : ".$offre->experience_offre_emploi."</p>");
 			print("<p> Description de l'offre : ".$offre->description_offre_emploi."</p>");
+			print("<p> Vous avez postuler à cette offre le : ".$this->changeDateNaissance($postuler->date_postule)."</p>");
 			echo CHtml::link('Voir cette offre' ,array('offreEmploi/view', 'id'=>$offre->id_offre_emploi));
 			echo "<hr/>";
 			$aPostuler = true;
